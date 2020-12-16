9126949cb329857c9d8e69475a2167b603ebcf33
wep1980 <wepbike@gmail.com>
2020-12-16 10:26:21 -0300

    Refazendo um pedido anterior

--- src/main/java/br/com/waldirep/bluefood/infrastructure/web/controller/CarrinhoController.java
+++ src/main/java/br/com/waldirep/bluefood/infrastructure/web/controller/CarrinhoController.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import br.com.waldirep.bluefood.domain.pedido.Carrinho;
+import br.com.waldirep.bluefood.domain.pedido.ItemPedido;
+import br.com.waldirep.bluefood.domain.pedido.Pedido;
+import br.com.waldirep.bluefood.domain.pedido.PedidoRepository;
 import br.com.waldirep.bluefood.domain.pedido.RestauranteDiferenteException;
 import br.com.waldirep.bluefood.domain.restaurante.ItemCardapio;
 import br.com.waldirep.bluefood.domain.restaurante.ItemCardapioRepository;
@@ -27,6 +30,9 @@ public class CarrinhoController {
 
 	@Autowired
 	private ItemCardapioRepository itemCardapioRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
 	
 	
 	
@@ -99,7 +105,21 @@ public class CarrinhoController {
 	
 	
 	
-	
+	@GetMapping(path = "/refazerCarrinho")
+	public String refazerCarrinho(
+			@RequestParam("pedidoId") Integer pedidoId,
+			@ModelAttribute("carrinho") Carrinho carrinho,
+			Model model) {
+		
+		Pedido pedido = pedidoRepository.findById(pedidoId).orElseThrow();
+		
+		carrinho.limpar();
+		
+		for (ItemPedido itemPedido : pedido.getItens()) {
+			carrinho.adicionarItem(itemPedido);
+		}
+		return "cliente-carrinho";
+	}
 	
 	
 	
